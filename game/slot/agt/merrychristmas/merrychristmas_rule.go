1ff43299b8d5d841ed0dbb86564e35aba8544cdb
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 12:36:24 +0630

    Update merrychristmas_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/merrychristmas/merrychristmas_rule.go
+++ game/slot/agt/merrychristmas/merrychristmas_rule.go
@@ -3,7 +3,7 @@ package merrychristmas
 // See: https://agtsoftware.com/games/agt/christmas
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
