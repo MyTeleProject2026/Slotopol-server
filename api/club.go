eed7c58c764e67bb40d3346f087a910bf65e9af5
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-19 03:16:39 +0630

    Update club.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- api/club.go
+++ api/club.go
@@ -2,50 +2,42 @@ package api
 
 import (
 	"encoding/xml"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/MyTeleProject2026/Slotopol-server/config"
 )
 
-const (
-	sqlclub = "UPDATE club SET `bank`=`bank`+?, `fund`=`fund`+?, `lock`=`lock`+?, `utime`=CURRENT_TIMESTAMP WHERE `cid`=?"
-)
-
+// ApiClubList returns list of all clubs.
 func ApiClubList(c *gin.Context) {
-	type item struct {
-		XMLName xml.Name `json:"-" yaml:"-" xml:"club"`
-		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr"`
-		Name    string   `json:"name,omitempty" yaml:"name,omitempty" xml:"name,omitempty"`
-	}
 	var ret struct {
-		XMLName xml.Name `json:"-" yaml:"-" xml:"ret"`
-		List    []item   `json:"list" yaml:"list" xml:"list>club" form:"list"`
+		XMLName xml.Name           `json:"-" yaml:"-" xml:"ret"`
+		Clubs   []*ClubData `json:"clubs" yaml:"clubs" xml:"clubs>club"`
 	}
 
-	ret.List = make([]item, 0, Clubs.Len())
-	for cid, club := range Clubs.Items() {
-		ret.List = append(ret.List, item{CID: cid, Name: club.Name()})
+	var admin, al = MustAdmin(c, 0)
+	if admin == nil || al&ALadmin == 0 {
+		Ret403(c, AEC_club_list_noaccess, ErrNoAccess)
+		return
+	}
+
+	for _, club := range Clubs.Items() {
+		ret.Clubs = append(ret.Clubs, &club.data)
 	}
 
 	RetOk(c, ret)
 }
 
-// Returns current club state.
+// ApiClubIs checks if a club exists.
 func ApiClubIs(c *gin.Context) {
 	var err error
-	var ok bool
-	type item struct {
-		XMLName xml.Name `json:"-" yaml:"-" xml:"user"`
-		CID     uint64   `json:"cid,omitempty" yaml:"cid,omitempty" xml:"cid,attr,omitempty"`
-		Name    string   `json:"name,omitempty" yaml:"name,omitempty" xml:"name,attr,omitempty"`
-	}
 	var arg struct {
 		XMLName xml.Name `json:"-" yaml:"-" xml:"arg"`
-		List    []item   `json:"list" yaml:"list" xml:"list>user" form:"list" binding:"required"`
+		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr" form:"cid" binding:"required"`
 	}
 	var ret struct {
 		XMLName xml.Name `json:"-" yaml:"-" xml:"ret"`
-		List    []item   `json:"list" yaml:"list" xml:"list>user"`
+		Exists  bool     `json:"exists" yaml:"exists" xml:"exists"`
 	}
 
 	if err = c.ShouldBind(&arg); err != nil {
@@ -53,31 +45,17 @@ func ApiClubIs(c *gin.Context) {
 		return
 	}
 
-	ret.List = make([]item, len(arg.List))
-	for i, ai := range arg.List {
-		var ri item
-		if ai.CID != 0 {
-			var club *Club
-			if club, ok = Clubs.Get(ai.CID); ok {
-				ri.CID = club.CID()
-				ri.Name = club.Name()
-			}
-		} else {
-			for _, club := range Clubs.Items() {
-				if name := club.Name(); name == ai.Name {
-					ri.CID = club.CID()
-					ri.Name = name
-					break
-				}
-			}
-		}
-		ret.List[i] = ri
+	var admin, al = MustAdmin(c, arg.CID)
+	if admin == nil || al&ALadmin == 0 {
+		Ret403(c, AEC_club_is_noaccess, ErrNoAccess)
+		return
 	}
 
+	_, ret.Exists = Clubs.Get(arg.CID)
 	RetOk(c, ret)
 }
 
-// Returns current club state.
+// ApiClubInfo returns detailed information about a club.
 func ApiClubInfo(c *gin.Context) {
 	var err error
 	var ok bool
@@ -86,8 +64,8 @@ func ApiClubInfo(c *gin.Context) {
 		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr" form:"cid" binding:"required"`
 	}
 	var ret struct {
-		XMLName  xml.Name `json:"-" yaml:"-" xml:"ret"`
-		ClubData `yaml:",inline"`
+		XMLName xml.Name `json:"-" yaml:"-" xml:"ret"`
+		Club    ClubData `json:"club" yaml:"club" xml:"club"`
 	}
 
 	if err = c.ShouldBind(&arg); err != nil {
@@ -97,32 +75,28 @@ func ApiClubInfo(c *gin.Context) {
 
 	var club *Club
 	if club, ok = Clubs.Get(arg.CID); !ok {
-		Ret500(c, AEC_club_info_noclub, ErrNoClub)
+		Ret404(c, AEC_club_info_noclub, ErrNoClub)
 		return
 	}
 
-	var _, al = MustAdmin(c, arg.CID)
-	if al&ALmaster == 0 {
+	var admin, al = MustAdmin(c, arg.CID)
+	if admin == nil || al&ALadmin == 0 {
 		Ret403(c, AEC_club_info_noaccess, ErrNoAccess)
 		return
 	}
 
-	ret.ClubData = club.Get()
-
+	ret.Club = club.Get()
 	RetOk(c, ret)
 }
 
-// Returns full jackpot value.
+// ApiClubJpfund updates the jackpot fund for a club.
 func ApiClubJpfund(c *gin.Context) {
 	var err error
 	var ok bool
 	var arg struct {
 		XMLName xml.Name `json:"-" yaml:"-" xml:"arg"`
 		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr" form:"cid" binding:"required"`
-	}
-	var ret struct {
-		XMLName xml.Name `json:"-" yaml:"-" xml:"ret"`
-		JpFund  float64  `json:"jpfund" yaml:"jpfund" xml:"jpfund"`
+		Fund    float64  `json:"fund" yaml:"fund" xml:"fund" binding:"required"`
 	}
 
 	if err = c.ShouldBind(&arg); err != nil {
@@ -132,23 +106,38 @@ func ApiClubJpfund(c *gin.Context) {
 
 	var club *Club
 	if club, ok = Clubs.Get(arg.CID); !ok {
-		Ret500(c, AEC_club_jpfund_noclub, ErrNoClub)
+		Ret404(c, AEC_club_jpfund_noclub, ErrNoClub)
 		return
 	}
 
-	ret.JpFund = club.Fund()
+	var admin, al = MustAdmin(c, arg.CID)
+	if admin == nil || al&ALmaster == 0 {
+		Ret403(c, AEC_club_jpfund_noaccess, ErrNoAccess)
+		return
+	}
 
-	RetOk(c, ret)
+	// Update fund in memory
+	club.AddFund(arg.Fund - club.Fund())
+
+	// Persist to database via BankBat
+	if Cfg.ClubInsertBuffer > 1 {
+		go BankBat[arg.CID].Fund(XormStorage, arg.Fund)
+	} else if err = BankBat[arg.CID].Fund(XormStorage, arg.Fund); err != nil {
+		Ret500(c, AEC_club_jpfund_sql, err)
+		return
+	}
+
+	Ret204(c)
 }
 
-// Rename the club.
+// ApiClubRename changes the name of a club.
 func ApiClubRename(c *gin.Context) {
 	var err error
 	var ok bool
 	var arg struct {
 		XMLName xml.Name `json:"-" yaml:"-" xml:"arg"`
 		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr" form:"cid" binding:"required"`
-		Name    string   `json:"name" yaml:"name" xml:"name" form:"name" binding:"required"`
+		Name    string   `json:"name" yaml:"name" xml:"name" binding:"required"`
 	}
 
 	if err = c.ShouldBind(&arg); err != nil {
@@ -158,113 +147,71 @@ func ApiClubRename(c *gin.Context) {
 
 	var club *Club
 	if club, ok = Clubs.Get(arg.CID); !ok {
-		Ret500(c, AEC_club_rename_noclub, ErrNoClub)
+		Ret404(c, AEC_club_rename_noclub, ErrNoClub)
 		return
 	}
 
-	var _, al = MustAdmin(c, arg.CID)
-	if al&ALmaster == 0 {
+	var admin, al = MustAdmin(c, arg.CID)
+	if admin == nil || al&ALadmin == 0 {
 		Ret403(c, AEC_club_rename_noaccess, ErrNoAccess)
 		return
 	}
 
-	if _, err = cfg.XormStorage.ID(club.CID()).Cols("name").Update(&ClubData{Name: arg.Name}); err != nil {
-		Ret500(c, AEC_club_rename_update, err)
+	// Update name in memory
+	club.SetName(arg.Name)
+
+	// Update in database
+	if _, err = XormStorage.ID(arg.CID).Cols("name").Update(&ClubData{Name: arg.Name}); err != nil {
+		Ret500(c, AEC_club_rename_sql, err)
 		return
 	}
-
-	club.SetName(arg.Name)
 
 	Ret204(c)
 }
 
-// Adding or withdrawing coins from club bank, fund and lock balances.
+// ApiClubCashin moves money to/from club deposit.
 func ApiClubCashin(c *gin.Context) {
 	var err error
 	var ok bool
 	var arg struct {
 		XMLName xml.Name `json:"-" yaml:"-" xml:"arg"`
 		CID     uint64   `json:"cid" yaml:"cid" xml:"cid,attr" form:"cid" binding:"required"`
-		BankSum float64  `json:"banksum" yaml:"banksum" xml:"banksum" form:"banksum"`
-		FundSum float64  `json:"fundsum" yaml:"fundsum" xml:"fundsum" form:"fundsum"`
-		LockSum float64  `json:"locksum" yaml:"locksum" xml:"locksum" form:"locksum"`
-	}
-	var ret struct {
-		XMLName xml.Name `json:"-" yaml:"-" xml:"ret"`
-		BID     uint64   `json:"bid" yaml:"bid" xml:"bid,attr"`
-		Bank    float64  `json:"bank" yaml:"bank" xml:"bank"`
-		Fund    float64  `json:"fund" yaml:"fund" xml:"fund"`
-		Lock    float64  `json:"lock" yaml:"lock" xml:"lock"`
+		Sum     float64  `json:"sum" yaml:"sum" xml:"sum" binding:"required"`
 	}
 
 	if err = c.ShouldBind(&arg); err != nil {
 		Ret400(c, AEC_club_cashin_nobind, err)
 		return
 	}
-	if arg.BankSum == 0 && arg.FundSum == 0 && arg.LockSum == 0 {
-		Ret400(c, AEC_club_cashin_nosum, ErrNoAddSum)
+	if arg.Sum > Cfg.AdjunctLimit || arg.Sum < -Cfg.AdjunctLimit {
+		Ret400(c, AEC_club_cashin_limit, ErrTooBig)
 		return
 	}
 
 	var club *Club
 	if club, ok = Clubs.Get(arg.CID); !ok {
-		Ret500(c, AEC_club_cashin_noclub, ErrNoClub)
+		Ret404(c, AEC_club_cashin_noclub, ErrNoClub)
 		return
 	}
 
-	var _, al = MustAdmin(c, arg.CID)
-	if al&ALmaster == 0 {
+	var admin, al = MustAdmin(c, arg.CID)
+	if admin == nil || al&ALmaster == 0 {
 		Ret403(c, AEC_club_cashin_noaccess, ErrNoAccess)
 		return
 	}
 
-	var bank, fund, lock = club.GetCash()
-	bank += arg.BankSum
-	fund += arg.FundSum
-	lock += arg.LockSum
+	// Update memory
+	club.AddDeposit(arg.Sum)
+	club.AddBank(-arg.Sum) // deposit taken from bank
 
-	if bank < 0 {
-		Ret403(c, AEC_club_cashin_bankout, ErrBankOut)
-		return
-	}
-	if fund < 0 {
-		Ret403(c, AEC_club_cashin_fundout, ErrFundOut)
-		return
-	}
-	if lock < 0 {
-		Ret403(c, AEC_club_cashin_lockout, ErrLockOut)
-		return
-	}
-
-	var rec = Banklog{
-		Bank:    bank,
-		Fund:    fund,
-		Lock:    lock,
-		BankSum: arg.BankSum,
-		FundSum: arg.FundSum,
-		LockSum: arg.LockSum,
-	}
-	if err = SafeTransaction(cfg.XormStorage, func(session *Session) (err error) {
-		if _, err = session.Exec(sqlclub, arg.BankSum, arg.FundSum, arg.LockSum, club.CID()); err != nil {
-			Ret500(c, AEC_club_cashin_sqlbank, err)
-			return
-		}
-		if _, err = session.Insert(&rec); err != nil {
-			Ret500(c, AEC_club_cashin_sqllog, err)
-			return
-		}
-		return
+	// Persist via BankBat (we need to define BankBat.Cashin or use direct SQL)
+	if err = SafeTransaction(XormStorage, func(session *Session) error {
+		_, err := session.Exec("UPDATE club SET bank=bank-?, lock=lock+? WHERE cid=?", arg.Sum, arg.Sum, arg.CID)
+		return err
 	}); err != nil {
+		Ret500(c, AEC_club_cashin_sql, err)
 		return
 	}
 
-	// make changes to memory data
-	club.AddCash(arg.BankSum, arg.FundSum, arg.LockSum)
-
-	ret.BID = rec.ID
-	ret.Bank = bank
-	ret.Fund = fund
-	ret.Lock = lock
-
-	RetOk(c, ret)
+	Ret204(c)
 }
