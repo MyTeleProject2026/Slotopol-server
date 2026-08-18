d009cfdecbe530208bc4319136e3a74502d47799
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 07:48:50 +0630

    Update aislot_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/aislot/aislot_rule.go
+++ game/slot/agt/aislot/aislot_rule.go
@@ -3,7 +3,7 @@ package aislot
 // See: https://agtsoftware.com/games/agt/aislot
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
