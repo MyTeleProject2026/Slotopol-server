7bf3224971c1be8902119518eab2e963253b2f7e
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-19 03:11:42 +0630

    Update links.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- cmd/links.go
+++ cmd/links.go
@@ -2,11 +2,11 @@ package cmd
 
 import (
 	// Keno
-	_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/americankeno"
-	_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/firekeno"
-	_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenocenturion"
-	_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenofast"
-	_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenoluxury"
+	//_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/americankeno"
+	//_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/firekeno"
+	//_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenocenturion"
+	//_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenofast"
+	//_ "github.com/MyTeleProject2026/Slotopol-server/game/keno/kenoluxury"
 
 	// AGT
 	_ "github.com/MyTeleProject2026/Slotopol-server/game/slot/agt/aislot"
