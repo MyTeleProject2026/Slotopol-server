a9d163a5db191ebca6d9da9f2e4b2257b5158b50
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 12:24:13 +0630

    Update icefruits_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/icefruits/icefruits_rule.go
+++ game/slot/agt/icefruits/icefruits_rule.go
@@ -3,7 +3,7 @@ package icefruits
 // See: https://agtsoftware.com/games/agt/megaice
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
