b9eff0e0596b22d9c8cd0bc23fda045555b1ae01
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 10:41:11 +0630

    Update cherryhot_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/cherryhot/cherryhot_rule.go
+++ game/slot/agt/cherryhot/cherryhot_rule.go
@@ -3,7 +3,7 @@ package cherryhot
 // See: https://agtsoftware.com/games/agt/cherryhot
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
