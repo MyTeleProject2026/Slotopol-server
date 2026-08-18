5f5a737ec1731652bb515f4931fe086d519f6861
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 07:47:28 +0630

    Update santa_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/santa/santa_rule.go
+++ game/slot/agt/santa/santa_rule.go
@@ -3,7 +3,7 @@ package santa
 // See: https://agtsoftware.com/games/agt/santa
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
