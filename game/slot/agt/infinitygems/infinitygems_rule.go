5f92ecc6068efcc95b8d488e0920d76d182b95ab
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-18 12:31:26 +0630

    Update infinitygems_rule.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- game/slot/agt/infinitygems/infinitygems_rule.go
+++ game/slot/agt/infinitygems/infinitygems_rule.go
@@ -3,7 +3,7 @@ package infinitygems
 // See: https://agtsoftware.com/games/agt/infinitygems
 
 import (
-	"github.com/slotopol/server/game/slot"
+	"github.com/MyTeleProject2026/Slotopol-server/game/slot"
 )
 
 const (
