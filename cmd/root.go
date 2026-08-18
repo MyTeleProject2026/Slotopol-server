1e71ac3ed707840b19ee8dc65e69cdfcd526b28d
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-19 02:37:48 +0630

    Update root.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -14,22 +14,22 @@ const rootLong = `This application implements web server and reels scanner for s
 
 var (
 	rootCmd = &cobra.Command{
-		Use:     cfg.AppName,
-		Version: cfg.BuildVers,
+		Use:     config.AppName,
+		Version: config.BuildVers,
 		Short:   rootShort,
 		Long:    rootLong,
 	}
 )
 
 func init() {
-	cobra.OnInitialize(cfg.InitConfig)
+	cobra.OnInitialize(config.InitConfig)
 
 	var pf = rootCmd.PersistentFlags()
-	pf.StringVarP(&cfg.CfgFile, "config", "c", "", "config file (default is config/slot-app.yaml at executable location)")
-	pf.StringVarP(&cfg.SqlPath, "sqlite", "q", "", "sqlite databases path (default same as config file path)")
-	pf.StringArrayVarP(&cfg.ObjPath, "fpath", "f", nil, "additional paths to yaml files or folders with game specific data (can be repeated)")
-	pf.BoolVarP(&cfg.Verbose, "verbose", "v", false, "print more verbose information to log")
-	rootCmd.SetVersionTemplate(fmt.Sprintf("version: %s, builton: %s", cfg.BuildVers, cfg.BuildTime))
+	pf.StringVarP(&config.CfgFile, "config", "c", "", "config file (default is config/slot-app.yaml at executable location)")
+	pf.StringVarP(&config.SqlPath, "sqlite", "q", "", "sqlite databases path (default same as config file path)")
+	pf.StringArrayVarP(&config.ObjPath, "fpath", "f", nil, "additional paths to yaml files or folders with game specific data (can be repeated)")
+	pf.BoolVarP(&config.Verbose, "verbose", "v", false, "print more verbose information to log")
+	rootCmd.SetVersionTemplate(fmt.Sprintf("version: %s, builton: %s", config.BuildVers, config.BuildTime))
 }
 
 // Execute executes the root command.
