8e18e7c2ecc290bf12e765a34f872f197d7e6013
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-20 14:13:33 +0630

    Update build-docker.sh
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- task/build-docker.sh
+++ task/build-docker.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 set -euxo pipefail
 
-wd=$(realpath -s "$(dirname "$0")/..")
+wd="$(realpath -s "$(dirname "$0")/..")"
 
 mkdir -p "$GOPATH/bin/config" "$GOPATH/bin/sqlite"
 
 cp -ruv "$wd/appdata/"* "$GOPATH/bin/config"
 
 buildvers="v0.12.0"
-buildtime=$(date +'%FT%T.%3NZ')
+buildtime="$(date +'%FT%T.%3NZ')"
 
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=1
 
+echo "===== BUILD ENVIRONMENT ====="
 go version
-go env GOOS GOARCH CGO_ENABLED
+go env GOOS GOARCH CGO_ENABLED CC CXX
+
+echo "===== C COMPILER ====="
+which gcc || true
+gcc --version || true
+
+ldflags="-w -s -linkmode external -extldflags=-static"
+ldflags="$ldflags -X github.com/MyTeleProject2026/Slotopol-server/config.BuildVers=$buildvers"
+ldflags="$ldflags -X github.com/MyTeleProject2026/Slotopol-server/config.BuildTime=$buildtime"
+
+echo "===== GO BUILD ====="
 
 go build \
   -o /go/bin/app \
@@ -23,7 +34,8 @@ go build \
   -tags="jsoniter prod full" \
   -buildvcs=false \
   -trimpath \
-  -ldflags="-w -s -linkmode external -extldflags '-static' \
--X github.com/MyTeleProject2026/Slotopol-server/config.BuildVers=$buildvers \
--X github.com/MyTeleProject2026/Slotopol-server/config.BuildTime=$buildtime" \
+  -ldflags="$ldflags" \
   "$wd"
+
+echo "===== BUILD SUCCESS ====="
+ls -lh /go/bin/app
