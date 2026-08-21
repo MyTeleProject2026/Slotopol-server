8b513da79e377e8dd81bf25973a5675c4519ea59
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-21 07:31:23 +0630

    Update strutil.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

--- util/strutil.go
+++ util/strutil.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"math/rand"
 	"os"
+	"time"
 	"unsafe"
 )
 
@@ -418,4 +420,15 @@ func Envfmt(fpath string, envmap map[string]string) string {
 		copy(e[j:], b[pc:])
 	}
 	return B2S(e)
+}
+
+// RandString generates a random string of given length.
+func RandString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[r.Intn(len(letters))]
+	}
+	return string(b)
 }
