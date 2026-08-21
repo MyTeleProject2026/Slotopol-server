ae8341dd22417415a2bcb286e90123dd4082e3f0
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-21 16:14:46 +0630

    Create embed.go
    Signed-off-by: CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>

---
+++ game/embed.go
@@ -0,0 +1,24 @@
+package game
+
+import (
+	"embed"
+	"io/fs"
+	"path/filepath"
+)
+
+//go:embed slot/*/*.yaml
+var embeddedYAML embed.FS
+
+func init() {
+	_ = fs.WalkDir(embeddedYAML, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil || d.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) != ".yaml" {
+			return nil
+		}
+		data, _ := embeddedYAML.ReadFile(path)
+		LoadMap = append(LoadMap, data)
+		return nil
+	})
+}
